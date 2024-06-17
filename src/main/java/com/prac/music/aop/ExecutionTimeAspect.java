ab838980937006714c3a32461ffe7a1c40384622
Ham-Seung-Wan <syham001@naver.com>
2024-06-17 10:18:29 +0900

    aop수정

--- src/main/java/com/prac/music/aop/ExecutionTimeAspect.java
+++ src/main/java/com/prac/music/aop/ExecutionTimeAspect.java
@@ -1,19 +1,32 @@
 package com.prac.music.aop;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 @Aspect
+@Slf4j
 public class ExecutionTimeAspect {
-    @Around("@annotation(measureExecutionTime)")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint, MeasureExecutionTime measureExecutionTime) throws Throwable {
-        long startTime = System.currentTimeMillis();
+    @Around("execution(* com.prac..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+//        long startTime = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
-        long endTime = System.currentTimeMillis();
-        System.out.println("{" + joinPoint.getSignature() + "}" + "executed in {" + (endTime - startTime) + "} ms");
-        return proceed;
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("{" + joinPoint.getSignature() + "}" + "executed in {" + (endTime - startTime) + "} ms");
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = requestAttributes.getRequest();
+            String method = request.getMethod();
+            String url = request.getRequestURL().toString();
+            log.info(method+"    "+url);
+        }
+
+            return proceed;
     }
 }
